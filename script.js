834442a4e59c67574ca4c59fbfab91393d84fb2f
ikeriano <rodrigueztejedoriker89@gmail.com>
2025-03-24 14:24:51 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('colorPicker').addEventListener('input', function() {
+    document.body.style.backgroundColor = this.value;
+});document.getElementById('colorPicker').addEventListener('input', function() {
+    document.body.style.backgroundColor = this.value;
+});
